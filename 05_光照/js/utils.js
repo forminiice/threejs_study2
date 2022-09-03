49d076afaad072c333622a34bb9be1545c85aed9
miniice <miniice@iice.fun>
2022-09-03 18:01:07 +0800

    添加光照

---
+++ 05_光照/js/utils.js
@@ -0,0 +1,28 @@
+import * as THREE from "three";
+
+// 创建对象实例时传入的 object 和 prop，返回一个十六进制色值的字符串
+export class ColorGUIHelper {
+  constructor(object, prop) {
+    this.object = object;
+    this.prop = prop;
+  }
+  get value() {
+    return `#${this.object[this.prop].getHexString()}`;
+  }
+  set value(hexString) {
+    this.object[this.prop].set(hexString);
+  }
+}
+// 提供一个可以以度数为单位进行操作的对象
+export class DegRadHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+  }
+  set value(v) {
+    this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+  }
+}
