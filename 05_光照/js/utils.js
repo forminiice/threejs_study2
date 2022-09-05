f00dd19a101de0aea20074471dee6976cd00227d
miniice <miniice@iice.fun>
2022-09-05 17:22:26 +0800

    添加工具类

--- 05_光照/js/utils.js
+++ 05_光照/js/utils.js
@@ -1,5 +1,32 @@
 import * as THREE from "three";
 
+// 提供一个可以以度数为单位进行操作的对象
+export class DegRadHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+  }
+  set value(v) {
+    this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+  }
+}
+// 将 "123" 这样的字符串转换为 123 这样的数字
+export class StringToNumberHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return this.obj[this.prop];
+  }
+  set value(v) {
+    this.obj[this.prop] = parseFloat(v);
+  }
+}
+
 // 创建对象实例时传入的 object 和 prop，返回一个十六进制色值的字符串
 export class ColorGUIHelper {
   constructor(object, prop) {
